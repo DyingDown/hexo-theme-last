ed6e0302011c87f0325109a7ffc1cb05a3737a3f
DyingDown <o_oyao@outlook.com>
2025-03-11 16:17:06 -0400

    fix: toc not fixed

--- source/js/last.js
+++ source/js/last.js
@@ -29,55 +29,6 @@ function insertAfter(newElement, targetElement) { // newElement是要追加的�
 // }
 
 
-/********** toc fix **********/
-
-
-menuIcon.onclick = function() {
-    var first = this.childNodes[0];
-    var second = this.childNodes[1];
-    var third = this.childNodes[2];
-    var nav = document.getElementById("nav");
-    if (menuClickFlag == 0) {
-        modifyClass(second, "newSecondLine", 1);
-        modifyClass(first, "newFirstLine", 1);
-        modifyClass(third, "newThirdLine", 1);
-        modifyClass(nav, "newNav", 1);
-        modifyClass(menuOuter, "newMenuOuter", 1);
-        menuClickFlag = 1;
-    } else {
-        modifyClass(second, "newSecondLine", 0);
-        modifyClass(first, "newFirstLine", 0);
-        modifyClass(third, "newThirdLine", 0);
-        modifyClass(nav, "newNav", 0);
-        menuClickFlag = 0;
-        modifyClass(menuOuter, "newMenuOuter", 0);
-    }
-
-}
-
-
-window.addEventListener("scroll", function() {
-    // if (isHome) {
-    if (this.window.scrollY > 0) {
-        modifyClass(menuOuter, "newMenuOuterColor", 1);
-    } else {
-        modifyClass(menuOuter, "newMenuOuterColor", 0);
-    }
-    // }
-    /******** fix toc *******/
-    let s = document.body.scrollTop || document.documentElement.scrollTop;
-    if (typeof(toc) != 'undefined') {
-        if (s > H - 100) {
-            let sidebar = document.getElementById("toc-col");
-            let width = sidebar.offsetWidth;
-            toc.style = "position:fixed;top:50px;width:" + width + "px";
-        } else {
-            toc.style = "";
-        }
-    }
-
-});
-
 /****** go to top button ******/
 var backToTopButton = document.getElementById("back-to-top")
 var toCommentButton = document.getElementById("go-to-comment")
